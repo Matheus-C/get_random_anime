da3ead1f7aa22c44b539afa0fa7068217b27d9f5
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-08-27 14:00:24 -0300

    tentando fazer progresso com a api do MyAnimeList

--- modules/express.js
+++ modules/express.js
@@ -4,10 +4,46 @@ const app = express();
 
 app.use(express.json());
 
+const CLIENT_ID = "76f967452b8f6c2202aa934b10a39800";
+const CLIENT_SECRET = "c73f92b68c6298b1a0e0480c9b593bf938783ff9c77ed1c89ce27020fb5d751d";
+
+const randomstring = require("randomstring");
+const crypto = require("crypto");
+const base64url = require("base64url");
+
+const code_verifier = randomstring.generate(128);
+
+const base64Digest = crypto
+  .createHash("sha256")
+  .update(code_verifier)
+  .digest("base64");
+
+console.log(base64Digest);
+
+const code_challenge = base64url.fromBase64(base64Digest);
+
+console.log(code_challenge);
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
 
+app.get("/auth", (req, res)=>{
+    res.redirect(`https://myanimelist.net/v2/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&code_challenge=${code_challenge}&state=requestauth`);
+});
+
+app.post("/oauth", (req, res) => {
+    const url = "https://myanimelist.net/v2/oauth2/token";
+    const data = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'code': req.body.authorisation_code,
+        'code_verifier': code_verifier,
+        'grant_type': 'authorization_code'
+    };
+
+    const response = req.post(url, data);
+    const token = response.json();
+});
 
 app.get("/", (req, res) => {
     res.render("index");
