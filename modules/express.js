a746fdcd5d35c3d8b890941478836269ca035612
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-08-27 17:23:41 -0300

    WIP fazendo alguns ajustes na autenticação

--- modules/express.js
+++ modules/express.js
@@ -7,6 +7,7 @@ app.use(express.json());
 const CLIENT_ID = "76f967452b8f6c2202aa934b10a39800";
 const CLIENT_SECRET = "c73f92b68c6298b1a0e0480c9b593bf938783ff9c77ed1c89ce27020fb5d751d";
 
+
 const randomstring = require("randomstring");
 const crypto = require("crypto");
 const base64url = require("base64url");
@@ -28,21 +29,27 @@ app.set("view engine", "ejs");
 app.set("views", "src/views");
 
 app.get("/auth", (req, res)=>{
-    res.redirect(`https://myanimelist.net/v2/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&code_challenge=${code_challenge}&state=requestauth`);
+    res.redirect(`https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&code_challenge=${code_challenge}&state=requestauth`);
 });
 
-app.post("/oauth", (req, res) => {
-    const url = "https://myanimelist.net/v2/oauth2/token";
+app.get("/oauth", async (req, res) => {
+    const url = "https://myanimelist.net/v1/oauth2/token";
     const data = {
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
-        'code': req.body.authorisation_code,
+        'code': req.query.code,
         'code_verifier': code_verifier,
         'grant_type': 'authorization_code'
     };
-
-    const response = req.post(url, data);
+    try{
+    const response = await fetch(url, {method: "POST", 
+    body: data}).then(response=>response.json()).then(response => console.log(JSON.stringify(response)));
     const token = response.json();
+    res.redirect("/");
+    }catch(error){
+        console.log(error.message);
+        res.redirect("/");
+    }
 });
 
 app.get("/", (req, res) => {
@@ -51,18 +58,18 @@ app.get("/", (req, res) => {
 
 app.get("/getanime", (req, res) => {
     try{
-        const data = fetch(`https://api.myanimelist.net/v2/users/${user_name}/animelist?fields=list_status=plan_to_watch&limit=1000`).then((res)=>{
+        const data = fetch(`https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status=plan_to_watch&limit=1000`, headers = {
+            'Authorization': `Bearer {access_token}`}).then((res)=>{
         if (res.status === 200){
-            return res.json();
+            const selected = Math.floor(Math.random() * (data.length - 0 + 1) + 0);
+            return res.render("index", {anime: data[selected], token: token});
         }
         return console.log(`error status: ${res.status}`);
         });
     }catch(error){
         res.send(error.message);
     }
-    const selected = Math.floor(Math.random() * (data.length - 0 + 1) + 0);
     
-    res.render("index", {anime: data[selected]});
 });
 
 const port = 8080;
